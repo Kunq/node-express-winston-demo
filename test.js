f1781f1564c5cec74a981f5275014805b91b0660
kunq <Kun_Qian@msn.com>
2016-08-01 05:43:51 -0400

    first commit

---
+++ test.js
@@ -0,0 +1,4 @@
+var logger = require("./utils/logger");
+
+logger.info('winston2 - info logged');
+logger.error('winston2 - error logged');
---
+++ utils/logger.js
@@ -0,0 +1,33 @@
+var winston = require('winston');
+var appRoot = require('app-root-path');
+//winston.emitErrs = true;
+
+var logger = new winston.Logger({
+    transports: [
+	new winston.transports.File({
+            level: 'info',
+            filename: appRoot + '/logs/all-logs.log',
+            handleExceptions: true,
+            json: true,
+            maxsize: 5242880, //5MB
+            maxFiles: 5,
+            colorize: false,
+	    timestamp: true
+        }),
+        new winston.transports.Console({
+            level: 'debug',
+            handleExceptions: true,
+            json: false,
+            colorize: true
+        })
+    ],
+   // Application continue to execution even after an exception is caught
+    exitOnError: false
+});
+
+module.exports = logger;
+module.exports.stream = {
+    write: function(message, encoding){
+        logger.info(message);
+    }
+};
